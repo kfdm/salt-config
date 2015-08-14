ebc0a26a94c5a6c0e5af75e2dbfef07e2dbbd375
Paul Traylor <kungfudiscomonkey@gmail.com>
2015-08-14 21:09:07 +0900

    Minor updates to prep for push and pop funtionality

--- _states/json_manage.py
+++ _states/json_manage.py
@@ -6,12 +6,12 @@ __virtualname__ = 'json'
 def __virtual__():
     return __virtualname__
 
-def manage(name, context, indent=4):
+def manage(name, update={}, push={}, pop={}, **json_args):
     ret = {'name': name, 'changes': {}, 'result': False, 'comment': ''}
 
     with open(name, 'r+') as fp:
         config = json.load(fp)
-        for key, value in context.items():
+        for key, value in update.items():
            if key in config and config[key] != value:
                ret['changes'][key] = {'old': config[key], 'new': value}
                if not __opts__['test']:
@@ -23,11 +23,12 @@ def manage(name, context, indent=4):
         if __opts__['test']:
             ret['result'] = None
         else:
-            fp.seek(0)
-            json.dump(config, fp, indent=indent)
-            ret['comment'] = 'Wrote values to ' + name
+            if ret['changes']:
+                fp.seek(0)
+                json.dump(config, fp, **json_args)
+                ret['comment'] = 'Wrote values to ' + name
+            else:
+                ret['comment'] = 'No Changes'
             ret['result'] = True
 
     return ret
-
-
